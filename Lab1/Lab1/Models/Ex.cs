1f020c2125551b80ba2825f4bc0a97910405706f
asd12-gif <kieumytf@gmail.com>
2025-12-03 22:55:50 +0700

    Project Lab1

---
+++ Lab1/Lab1/Models/Ex.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab1.Models
+{
+    public class Ex
+    {
+        public string Message { get; set; } = "Chào mừng bạn đến với ASP.NET Core MVC!";
+    }
+}
---
+++ Lab1/Lab1/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
+    const toggleButton = document.getElementById('btnToggle');
+
+    toggleButton.addEventListener('click', () => {
+        // Thêm/Xóa class CSS 'dark-theme' trên thẻ body
+        body.classList.toggle('dark-theme');
+
+        // Thay đổi chữ trên nút (tùy chọn)
+        if (body.classList.contains('dark-theme')) {
+            toggleButton.textContent = "Bật Chế Độ Sáng";
+        } else {
+            toggleButton.textContent = "Bật/Tắt Chế Độ Tối";
+        }
+    });
+});
